8600685447cabafbfc9a01e8cd403757cf050ff9
Piotr Florczak <piotrflorczakk@gmail.com>
2020-11-14 19:12:18 +0100

    Dodanie stylów do szczegółow zapytania

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -20,6 +20,7 @@ function Results(props) {
       >
         <div className="result-header">
           <div className="short-details" onClick={() => handleClick(index)}>
+            <i className="arrow-icon"></i>
             <img src={Book} alt="Book" className="image" />
             <span className="title">{item.title}</span>
             <span className="author">{item.author}</span>
@@ -33,9 +34,28 @@ function Results(props) {
             <Tooltip text="Wysłanie prośby o wypożyczenie książki do właściciela." />
           </div>
         </div>
-        <div className="details">
-          <span className="title">{item.title}</span>
-          <span className="author">{item.author}</span>
+        <div className="result-details">
+          <img src={Book} alt="" />
+          <div className="details">
+            <div>
+              <h4>Tytuł</h4>
+              <span className="title">{item.title}</span>
+            </div>
+            <div>
+              <h4>Autor</h4>
+              <span className="author">{item.author}</span>
+            </div>
+            <div>
+              <h4>Lokalizacja</h4>
+              <span className="location">
+                <i className="location-icon"></i>Bielsko-Biała
+              </span>
+            </div>
+            <div>
+              <h4>Właściciel</h4>
+              <span className="owner">{item.owner.name}</span>
+            </div>
+          </div>
         </div>
       </div>
     ));
