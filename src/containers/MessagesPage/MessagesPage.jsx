0cc06e80157dda22c91f5e80039f1a202f89e6e7
Piotr Florczak <piotrflorczakk@gmail.com>
2020-12-13 16:00:20 +0100

    Dodanie komponentu Header, refaktoring

--- src/containers/MessagesPage/MessagesPage.jsx
+++ src/containers/MessagesPage/MessagesPage.jsx
@@ -1,15 +1,16 @@
 import Messages from "../../components/Messages/Messages";
+import Header from "../../components/Header/Header";
 import "./MessagesPage.scss";
 
 function MessagesPage() {
   return (
     <div className="message-page universal-background">
       <div className="container">
-        <h1>Wiadomości</h1>
-        <p>
-          Poniżej znajdziesz wiadomości odnośnie wypożyczonych książek przez lub
-          od Ciebie. Porozumiej się z innymi użytkownikami i uzgodnij szczegóły.
-        </p>
+        <Header
+          title="Wiadomości"
+          subtitle="Poniżej znajdziesz wiadomości odnośnie wypożyczonych książek przez lub
+          od Ciebie. Porozumiej się z innymi użytkownikami i uzgodnij szczegóły."
+        />
         <Messages />
       </div>
     </div>
--- src/containers/SearchPage/SearchPage.jsx
+++ src/containers/SearchPage/SearchPage.jsx
@@ -1,17 +1,18 @@
 import SearchBar from "../../components/SearchBar";
 import SearchResults from "../../components/SearchResults/SearchResults";
+import Header from "../../components/Header/Header";
 import "./SearchPage.scss";
 
 function SearchPage() {
   return (
     <div className="shearch-page universal-background">
       <div className="container">
-        <h1>Poszukaj książki w swojej okolicy</h1>
-        <p>
-          Każdy czytający książki wie, że zdobycie interesującej go pozycji nie
+        <Header
+          title="Poszukaj książki w swojej okolicy"
+          subtitle="Każdy czytający książki wie, że zdobycie interesującej go pozycji nie
           zawsze jest proste. W których inni użytkownicy mogą odnaleźć
-          interesujące ich tytuły. Każdy czytający książki wie, że zdobycie.
-        </p>
+          interesujące ich tytuły. Każdy czytający książki wie, że zdobycie."
+        />
         <SearchBar />
         <SearchResults />
       </div>
